f7df7490fae14a82efe5ef21ad13dc4eb1ef1696
peshk0v <nikita.peshkovvv@gmail.com>
2024-11-18 19:21:37 +0300

    Добавлена подсветка ответов в тесте

--- Test/post1.js
+++ Test/post1.js
@@ -1,33 +1,113 @@
 console.log("JS STARTED!!!");
 
-var ansvers = {"q1": "a","q2": "a", "q3": "a", "q4": "a", "q5": "a"}
+var ansvers = [2, 1, 1, 1, 1];
 
 function subl() {
     var score = 0;
-    
+    var q1TXTS = [document.getElementById("anst1_1"), document.getElementById("anst1_2"), document.getElementById("anst1_3")];
+    var q2TXTS = [document.getElementById("anst2_1"), document.getElementById("anst2_2"), document.getElementById("anst2_3")];
+    var q3TXTS = [document.getElementById("anst3_1"), document.getElementById("anst3_2"), document.getElementById("anst3_3")];
+    var q4TXTS = [document.getElementById("anst4_1"), document.getElementById("anst4_2"), document.getElementById("anst4_3")];
+    var q5TXTS = [document.getElementById("anst5_1"), document.getElementById("anst5_2"), document.getElementById("anst5_3")];
+
     var q1 = document.querySelector('input[name="q1"]:checked');
     var q2 = document.querySelector('input[name="q2"]:checked');
     var q3 = document.querySelector('input[name="q3"]:checked');
     var q4 = document.querySelector('input[name="q4"]:checked');
     var q5 = document.querySelector('input[name="q5"]:checked');
+    
+    if (q1 && q1.value === '' + ansvers[0]) {
+        score++;
+        q1TXTS[ansvers[0]-1].className = "anstxtT";
+        for (var i = 0; i < q1TXTS.length; i++) {
+            if (q1TXTS[i].className === "anstxt") {
+                q1TXTS[i].className = "anstxtF";
+            }
+        }
+    } else {
+        for (var i = 0; i < q1TXTS.length; i++) {
+            if (i === ansvers[0]-1) {
+                q1TXTS[i].className = "anstxtT";
+            } else {
+                q1TXTS[i].className = "anstxtF";
+            }
+        }
+    }
+    if (q2 && q2.value === '' + ansvers[1]) {
+        score++;
+        q2TXTS[ansvers[1]-1].className = "anstxtT";
+        for (var i = 0; i < q2TXTS.length; i++) {
+            if (q2TXTS[i].className === "anstxt") {
+                q2TXTS[i].className = "anstxtF";
+            }
+        }
+    } else {
+        for (var i = 0; i < q2TXTS.length; i++) {
+            if (i === ansvers[1]-1) {
+                q2TXTS[i].className = "anstxtT";
+            } else {
+                q2TXTS[i].className = "anstxtF";
+            }
+        }
+    }
+    if (q3 && q3.value === '' + ansvers[2]) {
+        score++;
+        q3TXTS[ansvers[2]-1].className = "anstxtT";
+        for (var i = 0; i < q3TXTS.length; i++) {
+            if (q3TXTS[i].className === "anstxt") {
+                q3TXTS[i].className = "anstxtF";
+            }
+        }
+    } else {
+        for (var i = 0; i < q3TXTS.length; i++) {
+            if (i === ansvers[2]-1) {
+                q3TXTS[i].className = "anstxtT";
+            } else {
+                q3TXTS[i].className = "anstxtF";
+            }
+        }
+    }
+    if (q4 && q4.value === '' + ansvers[3]) {
+        score++;
+        q4TXTS[ansvers[3]-1].className = "anstxtT";
+        for (var i = 0; i < q4TXTS.length; i++) {
+            if (q4TXTS[i].className === "anstxt") {
+                q4TXTS[i].className = "anstxtF";
+            }
+        }
+    } else {
+        for (var i = 0; i < q4TXTS.length; i++) {
+            if (i === ansvers[3]-1) {
+                q4TXTS[i].className = "anstxtT";
+            } else {
+                q4TXTS[i].className = "anstxtF";
+            }
+        }
+    }
+    if (q5 && q5.value === '' + ansvers[4]) {
+        score++;
+        q5TXTS[ansvers[4]-1].className = "anstxtT";
+        for (var i = 0; i < q5TXTS.length; i++) {
+            if (q5TXTS[i].className === "anstxt") {
+                q5TXTS[i].className = "anstxtF";
+            }
+        }
+    } else {
+        for (var i = 0; i < q5TXTS.length; i++) {
+            if (i === ansvers[4]-1) {
+                q5TXTS[i].className = "anstxtT";
+            } else {
+                q5TXTS[i].className = "anstxtF";
+            }
+        }
+    }
 
-    if (q1 && q1.value === ansvers["q1"]) {
-        score++;
-    }
-    if (q2 && q2.value === ansvers["q2"]) {
-        score++;
-    }
-    if (q3 && q3.value === ansvers["q3"]) {
-        score++;
-    }
-    if (q4 && q4.value === ansvers["q4"]) {
-        score++;
-    }
-    if (q5 && q5.value === ansvers["q5"]) {
-        score++;
-    }
+    var ansByUsr = confirm("Ваш результат: " + score + " из 5\nВы желаете посмотреть правильные ответы?");
 
-    alert("Ваш результат: " + score + " из 5");
+    if (ansByUsr) {} else {
+        window.location = "http://127.0.0.1:5500";
+    }
+    // alert("Ваш результат: " + score + " из 5");
 };
 
 console.log(ans1_1.value);
